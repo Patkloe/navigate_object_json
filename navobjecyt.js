b2d69fadfc45f9e1a41a762f1300ba539fa2c8bd
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-10-22 07:59:52 -0500

    Create navobjecyt.js

---
+++ navobjecyt.js
@@ -0,0 +1,9 @@
+var test = { 
+               a: "nouv",
+               b: "json",
+               c: "decouvrir"
+            };
+var getkeys = Object.keys(test);
+for(var i of getkeys)
+    alert("keys of the object : " + " " + i);
+ 
